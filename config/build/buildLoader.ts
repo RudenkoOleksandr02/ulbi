b5eea28804c9c23c0a2154233febca9dcbf72ba1
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-07-20 18:04:19 +0300

    1 04

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,6 +1,18 @@
 import webpack from "webpack";
 
 export function buildLoader(): webpack.RuleSetRule[] {
+    const styleLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }
+    // если не использовать тайпскрипт - нужен babel-loader
     const typescriptLoader = { // порядок лоудеров имеет значение, по хорошему их нужно выносить
         test: /\.tsx?$/, // файлы которые нужно пропустить через лоудеры
         use: 'ts-loader', // этот лоудер используется для typescript
@@ -8,5 +20,6 @@ export function buildLoader(): webpack.RuleSetRule[] {
     }
     return [
         typescriptLoader,
+        styleLoader
     ]
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-
-export function someFn(): void {
-    console.log('RANDOM FUNCTION 1111')
-}
