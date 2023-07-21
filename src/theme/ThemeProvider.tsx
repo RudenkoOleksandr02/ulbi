2485a788e6fc527484d0c6f34cf16b47266586fb
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-07-21 16:42:16 +0300

    1 07

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import React, {FC, useMemo, useState} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT; // as говорит расматривать как тип Theme
+const ThemeProvider: FC = ({children}) => { // FC - тип для функц компонент, children - пропс-внутрений компонент
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
