a3abcae9820f63c05de437ee44c835830700d780
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-07-24 23:11:10 +0300

    1 10

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -19,7 +19,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoaders(options) // лоудеры, нужны для сборки файлов которые не являются js или json
         },
-        resolve: buildResolvers(),
+        resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined, // делает карты исходного кода, по которым можно отследить в каком файде произошла ошибка
         devServer: isDev ? buildDevServer(options) : undefined
     }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import 'app/styles/index.scss'
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import { useTheme } from 'app/provider/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -16,8 +17,8 @@ const App = () => {
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<div>loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageAsync/>}/>
-                    <Route path='/' element={<MainPageAsync/>}/>
+                    <Route path='/about' element={<AboutPage/>}/>
+                    <Route path='/' element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
