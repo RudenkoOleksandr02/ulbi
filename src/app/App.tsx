f52af38ba7a8a149bab4726c0688a69d9aab0b2f
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-07-27 15:25:41 +0300

    2 12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import React, {Suspense, useContext, useState} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
 import 'app/styles/index.scss'
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import { useTheme } from 'app/provider/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import {AppRouter} from "app/provider/router";
+import {Navbar }from "widgets/Navbar";
 
 
 const App = () => {
@@ -13,10 +10,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>Toggle</button>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О сайте</Link>
-            {AppRouter}
         </div>
     );
 };
